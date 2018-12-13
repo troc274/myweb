9adba5f22c9c494cc448fefdd55ea6628994e338
Bao Nguyen <baonguyenyam@gmail.com>
2018-12-13 10:40:27 +0700

    do it

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,56 +1,85 @@
 'use strict';
 
 eval(function (p, a, c, k, _e, r) {
-    _e = function e(c) {
-        return (c < a ? '' : _e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36));
-    };if (!''.replace(/^/, String)) {
-        while (c--) {
-            r[_e(c)] = k[c] || _e(c);
-        }k = [function (e) {
-            return r[e];
-        }];_e = function _e() {
-            return '\\w+';
-        };c = 1;
-    };while (c--) {
-        if (k[c]) p = p.replace(new RegExp('\\b' + _e(c) + '\\b', 'g'), k[c]);
-    }return p;
+	_e = function e(c) {
+		return (c < a ? '' : _e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36));
+	};if (!''.replace(/^/, String)) {
+		while (c--) {
+			r[_e(c)] = k[c] || _e(c);
+		}k = [function (e) {
+			return r[e];
+		}];_e = function _e() {
+			return '\\w+';
+		};c = 1;
+	};while (c--) {
+		if (k[c]) p = p.replace(new RegExp('\\b' + _e(c) + '\\b', 'g'), k[c]);
+	}return p;
 }('3 k(c){4 7(9(c).d(/%([0-6-F]{2})/g,3 8(a,b){4 e.f(\'h\'+b)}))}3 5(a){4 i(j(a).G(\'\').l(3(c){4\'%\'+(\'m\'+c.n(0).o(p)).q(-2)}).r(\'\'))}s.t=3(a){u((a=a||v.w).x&&a.y&&a.z&&A==a.B)4 $("C"),D(5("E")),!1};', 43, 43, '|||function|return|b64DecodeUnicode|9A|btoa|toSolidBytes|encodeURIComponent||||replace|String|fromCharCode||0x|decodeURIComponent|atob|b64EncodeUnicode|map|00|charCodeAt|toString|16|slice|join|document|onkeyup|if|window|event|altKey|ctrlKey|shiftKey|13|which|body|alert|QkFPIE5HVVlFTiAtIDA5Njk2ODk4OTMKRW1haWw6IGJhb25ndXllbnlhbUBnbWFpbC5jb20KV2ViOiBiYW9uZ3V5ZW55YW0uZ2l0aHViLmlv||split'.split('|'), 0, {}));
 
 // Copyright 2014-2017 The Bootstrap Authors
 // Copyright 2014-2017 Twitter, Inc.
 if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-    var msViewportStyle = document.createElement('style');
-    msViewportStyle.appendChild(document.createTextNode('@-ms-viewport{width:auto!important}'));
-    document.head.appendChild(msViewportStyle);
+	var msViewportStyle = document.createElement('style');
+	msViewportStyle.appendChild(document.createTextNode('@-ms-viewport{width:auto!important}'));
+	document.head.appendChild(msViewportStyle);
 }
 
 $(function () {
-    var nua = navigator.userAgent;
-    var isAndroid = nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1;
-    if (isAndroid) {
-        $('select.form-control').removeClass('form-control').css('width', '100%');
-    }
+	var nua = navigator.userAgent;
+	var isAndroid = nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1;
+	if (isAndroid) {
+		$('select.form-control').removeClass('form-control').css('width', '100%');
+	}
 });
-var str = 'abcde';
-var pass = 'D1;S2;F3;G5;H6';
-var text = 'ac ba be de';
+var chuoi = 'qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM ';
+var mahoa = '!1/;!2=;!#3;!4;!5;!6;!7;!8;!9;!0;@#1;@2;@=3;@4;@5;@-6;@7;@^8;@9;@$0;_1;_2;_3;_4;_5;_6;!©/;!§=;!#®;!¢;!¡;!¥;!±;!µ;!¶;!£;@#©;@§;@=®;@¢;@¡;@-¥;@±;@^µ;@¶;@$£;_©;_§;_®;_¢;_¡;_¥;~';
+var m = chuoi.split('');
+var n = mahoa.split(';');
 
-var m = str.split('');
-var n = pass.split(';');
+function removeVietnam(s) {
+	var r = s;
+	var non_asciis = {
+		// '-': '[`~!@#$%^&*()_|+=?;:",.<>/]',
+		'a': '[ảàạảãàáâãäåắặẳằẵấầẩẫậâă]',
+		'ae': 'æ',
+		'c': 'ç',
+		'e': '[èéẹẽẻềệếểễê]',
+		'd': '[đ]',
+		'i': '[ìíîïị]',
+		'n': 'ñ',
+		'o': '[òóôõöộồốổỗơởợỡờớôơ]',
+		'oe': 'œ',
+		'u': '[ùúûűüủụưửựứừữư]',
+		'y': '[ýỳỷỵỹ]'
+	};
+	for (var i in non_asciis) {
+		r = r.replace(new RegExp(non_asciis[i], 'gi'), i);
+	}
+	// r = r.replace(/[^\w\s]/gi, '-')
+	return r;
+};
 
-for (var key in m) {
-    if (m.hasOwnProperty(key)) {
-        var element = m[key];
-        text = text.replace(new RegExp(element, 'g'), n[key]);
-    }
+function doMaHoa(str, e) {
+	var newstr = str;
+	for (var key in m) {
+		if (m.hasOwnProperty(key)) {
+			var element = m[key];
+			if (e) {
+				newstr = newstr.replace(new RegExp(n[key], 'g'), element);
+			} else {
+				newstr = newstr.replace(new RegExp(element, 'g'), n[key]);
+			}
+		}
+	}
+	return newstr;
 }
-console.log(text);
 
-for (var _key in m) {
-    if (m.hasOwnProperty(_key)) {
-        var _element = m[_key];
-        text = text.replace(new RegExp(n[_key], 'g'), _element);
-    }
-}
-console.log(text);
+$('#mahoa').on('click', function () {
+	var m = doMaHoa(removeVietnam($('#code').val()));
+	$('#exp').val(m);
+});
+$('#giaima').on('click', function () {
+	var m = doMaHoa($('#exp').val(), true);
+	$('#code').val(m);
+});
 //# sourceMappingURL=main.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,51 @@
-var str = 'abcde'
-var pass = 'D1;S2;F3;G5;H6'
-var text = 'ac ba be de'
+var chuoi = 'qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM '
+var mahoa = '!1/;!2=;!#3;!4;!5;!6;!7;!8;!9;!0;@#1;@2;@=3;@4;@5;@-6;@7;@^8;@9;@$0;_1;_2;_3;_4;_5;_6;!©/;!§=;!#®;!¢;!¡;!¥;!±;!µ;!¶;!£;@#©;@§;@=®;@¢;@¡;@-¥;@±;@^µ;@¶;@$£;_©;_§;_®;_¢;_¡;_¥;~'
+var m = chuoi.split('')
+var n = mahoa.split(';')
 
-var m = str.split('')
-var n = pass.split(';')
-
-for (const key in m) {
-	if (m.hasOwnProperty(key)) {
-		const element = m[key];
-		text = text.replace(new RegExp(element, 'g'), n[key])
+function removeVietnam(s) {
+	var r = s
+	var non_asciis = {
+		// '-': '[`~!@#$%^&*()_|+=?;:",.<>/]',
+		'a': '[ảàạảãàáâãäåắặẳằẵấầẩẫậâă]',
+		'ae': 'æ',
+		'c': 'ç',
+		'e': '[èéẹẽẻềệếểễê]',
+		'd': '[đ]',
+		'i': '[ìíîïị]',
+		'n': 'ñ',
+		'o': '[òóôõöộồốổỗơởợỡờớôơ]',
+		'oe': 'œ',
+		'u': '[ùúûűüủụưửựứừữư]',
+		'y': '[ýỳỷỵỹ]'
+	};
+	for (var i in non_asciis) {
+		r = r.replace(new RegExp(non_asciis[i], 'gi'), i);
 	}
-}
-console.log(text)
+	// r = r.replace(/[^\w\s]/gi, '-')
+	return r
+};
 
-
-for (const key in m) {
-	if (m.hasOwnProperty(key)) {
-		const element = m[key];
-		text = text.replace(new RegExp(n[key], 'g'), element)
+function doMaHoa(str, e) {
+	let newstr = str
+	for (let key in m) {
+		if (m.hasOwnProperty(key)) {
+			let element = m[key];
+			if(e) {
+				newstr = newstr.replace(new RegExp(n[key], 'g'), element)
+			} else {
+				newstr = newstr.replace(new RegExp(element, 'g'), n[key])
+			}
+		}
 	}
+	return newstr
 }
-console.log(text)
+
+$('#mahoa').on('click', function(){
+	var m = doMaHoa(removeVietnam($('#code').val()))
+	$('#exp').val(m)
+})
+$('#giaima').on('click', function(){
+	var m = doMaHoa($('#exp').val(), true)
+	$('#code').val(m)
+})
