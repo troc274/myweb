2f4c2540bb97a1e99cbb7a77950e2c69d53c84e8
Bao Nguyen <baonguyenyam@gmail.com>
2018-12-13 08:24:26 +0700

    npUP

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,16 +31,4 @@ $(function () {
         $('select.form-control').removeClass('form-control').css('width', '100%');
     }
 });
-// Main
-$(document).ready(function () {
-    // Phần Slider
-    $('.owl-carousel').owlCarousel({
-        items: 1,
-        nav: true,
-        dots: false,
-        padding: 30,
-        loop: true,
-        navText: ['<i class="fa fa-long-arrow-left"></i>', '<i class="fa fa-long-arrow-right"></i>']
-    });
-});
 //# sourceMappingURL=main.js.map
